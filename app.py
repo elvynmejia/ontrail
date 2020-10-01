cac7fd6004326149de997d2c1b16c41945c0f817
elvynmejia <elvynmejia@gmail.com>
2020-09-30 18:20:44 -0700

    Adds health check and leads endpoint

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 import config
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
-@app.route('/')
-def hello_world():
-    return os.environ.get('FLASK_ENV') or 'hello world'
+def health_check_ok():
+    return 'Ok'
+
+def health_check_boom():
+	  raise SystemError # change to internal error
+
+def leads():
+	 return jsonify({'id': 1, 'status': 'accepted'})
+
+# health API
+app.add_url_rule('/health/ok', view_func=health_check_ok, methods=['GET'])
+app.add_url_rule('/health/boom', view_func=health_check_boom, methods=['GET'])
+
+# leads API
+app.add_url_rule('/v1/leads', view_func=leads, methods=['GET'])
