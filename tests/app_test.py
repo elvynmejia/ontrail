3c40073b342af1a4be398c0ff3d641f08d1487fe
elvynmejia <elvynmejia@gmail.com>
2020-10-12 12:53:39 -0700

    Writes more tests

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,13 +1,23 @@
 from test_base import TestBase
+from db_config import db, migrate
+import pytest
 import pdb
 
-
 class TestApp(TestBase):
-    def test_config(self): 
+    def test_create_app(self, mocker):
+        mocker.patch("db_config.db.init_app")
+        mocker.patch("db_config.migrate.init_app")
         assert self.app.config.get("SQLALCHEMY_DATABASE_URI") is not None
         assert self.app.config.get("TESTING") == True
+        db.init_app.assert_called_once
+        migrate.init_app.assert_called_once
 
-    def test_health_check(self):
+    def test_health_check_ok(self):
         response = self.client.get("/health/ok")
         assert b"Ok" in response.data
         assert response.status_code == 200
+
+    def test_health_check_boom(self):
+        with pytest.raises(SystemError) as excinfo:
+        		self.client.get("/health/boom")
+        
