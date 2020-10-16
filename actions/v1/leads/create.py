c4d185835f971fcfa87613976a94630fee76966a
elvynmejia <elvynmejia@gmail.com>
2020-10-16 09:44:41 -0700

    Add test for lead create 422

--- actions/v1/leads/create.py
+++ actions/v1/leads/create.py
@@ -4,6 +4,7 @@ from marshmallow import ValidationError
 
 from repos import LeadRepo
 from entities import LeadEntity
+from actions.error import UnprocessableEntity
 
 
 class Create(MethodView):
@@ -11,24 +12,19 @@ class Create(MethodView):
         json_data = request.get_json()
         try:
             kwargs = {
-                "company_name": json_data["company_name"],
-                "position": json_data["position"],
-                "contacts": json_data["contacts"],
+                "company_name": json_data.get("company_name"),
+                "position": json_data.get("position"),
+                "contacts": json_data.get("contacts"),
                 "description": json_data.get("description"),
                 "status": json_data.get("status"),
             }
 
             data = LeadEntity().load(kwargs)
 
-        except ValidationError as err:
-            return (
-                {
-                    "errors": [err.messages],
-                    "message": "Unprocessable Entity",
-                    "code": "UNPROCESSABLE_ENTITY",
-                },
-                422,
-            )
+        except (ValidationError, KeyError) as err:
+            # this should be abstracted to an error handler
+            error = UnprocessableEntity(errors=[err.messages])
+            return error.as_json(), error.http_code
 
         lead = LeadRepo.create(**data)
         return ({"lead": lead.as_json()}, 201)
