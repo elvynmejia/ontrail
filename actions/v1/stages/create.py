39388c77eefc4cedfe4346354dd81f0e79c0219f
elvynmejia <elvynmejia@gmail.com>
2020-10-22 17:54:23 -0700

    Adds dates check to stage create action

--- actions/v1/stages/create.py
+++ actions/v1/stages/create.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask.views import MethodView, request
 from marshmallow import ValidationError
 
@@ -5,11 +6,26 @@ from repos import StageRepo, LeadRepo
 from repos.error import RecordNotFound
 from actions.error import UnprocessableEntity, NotFound
 from entities import StageEntity
+from constants import DATETIME_FORMAT
 
 
 class Create(MethodView):
     def post(self):
         json_data = request.get_json()
+
+        try:
+            start_at = datetime.strptime(
+                json_data.get("start_at"), DATETIME_FORMAT
+            ).isoformat()
+
+            end_at = datetime.strptime(
+                json_data.get("end_at"), DATETIME_FORMAT
+            ).isoformat()
+        except (ValueError, TypeError):
+            error = UnprocessableEntity(
+                message="Invalid start_at or end_at datetime format. Example datetime format allowed: 2020-10-22T16:53:37.697725"
+            )
+            return error.as_json(), error.http_code
 
         try:
             params = {
@@ -19,8 +35,8 @@ class Create(MethodView):
                 "notes": json_data.get("notes"),
                 "lead_id": json_data.get("lead_id"),
                 "state": json_data.get("state"),
-                "start_at": json_data.get("start_at"),
-                "end_at": json_data.get("end_at"),
+                "start_at": start_at,
+                "end_at": end_at,
             }
 
             data = StageEntity().load(params)
